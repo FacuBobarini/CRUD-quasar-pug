c676300e7c15086ee63e6ab568b29cae6efca8a9
Facu bobarini <facubobarini@gmail.com>
2024-10-19 01:33:39 -0300

    add new project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  nav
+    ul.navbar
+      li.nav-item
+        a(href="/create") Create
+      li.nav-item
+        a(href="/read") Read
+      li.nav-item
+        a(href="/update") Update
+      li.nav-item
+        a(href="/delete") Delete
+</template>
+  
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Navbar'
+});
+</script>
+  
+<style scoped> 
+.navbar {
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+  padding: 15px 0px;
+  margin: 0px;
+  background-color: #f8f9fa;
+}
+
+.nav-item {
+  margin: 0px 15px;
+}
+
+.nav-item a {
+  text-decoration: none;
+  color: #121212;
+  font-weight: bold;
+  padding: 10px 15px;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.nav-item a:hover {
+  background-color: #1976D2;
+  color: white; 
+}
+
+.nav-item a:active {
+  background-color: #1976D2;
+  color: white;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
-  router-view
+  q-page.row.items-center.justify-evenly
+    h1 CRUD Prueba
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'App'
+  name: 'Home',
+
+  components: {
+  },
+
+  setup () {
+  }
 });
 </script>
