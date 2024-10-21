0d3b97a3833994576cc46e779fd2d942191e0d55
Facu bobarini <facubobarini@gmail.com>
2024-10-20 22:16:52 -0300

    add component: form

---
+++ src/components/Form.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  form 
+    div.form-group
+      label(for="name") Nombre
+      input(id="name" placeholder="Escribe tu nombre").form-control
+    div.form-group
+      label(for="lastname") Apellido
+      input(id="lastname" placeholder="Escribe tu apellido").form-control
+    div.form-group
+      label(for="dni") DNI
+      input(id="dni" placeholder="Escribe tu DNI").form-control
+    button(type="submit").btn Submit
+</template>
+    
+<script lang="ts">
+import { defineComponent } from 'vue';
+  
+export default defineComponent({
+  name: 'Form'
+});
+</script>
+    
