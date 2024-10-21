0e678ac02f798c425536203e4976cc1d68a2aa16
Facu bobarini <facubobarini@gmail.com>
2024-10-20 22:55:33 -0300

    add create.vue page

---
+++ src/pages/Create.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  q-page.row.justify-evenly.page-container
+    Form
+</template>
+
+<script lang="ts">
+import Form from 'src/components/Form.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Create',
+
+  components: {
+    Form
+  },
+
+  setup () {
+  }
+});
+</script>
