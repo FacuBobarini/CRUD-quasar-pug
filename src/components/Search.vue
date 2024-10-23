d22cefe2849b7833f36f23a6d0f196c737450267
Facu bobarini <facubobarini@gmail.com>
2024-10-23 18:58:21 -0300

    add Search.vue file

---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  form 
+    div.form-group
+      label(for="search") Buscar por id
+      input(id="id" placeholder="Escribe el Id").form-control
+    button(type="submit").btn Buscar
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Search'
+})
+</script>
