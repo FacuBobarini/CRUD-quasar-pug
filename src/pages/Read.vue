544ed516a007d1d73d1872d939def2f54d75a539
Facu bobarini <facubobarini@gmail.com>
2024-10-23 22:33:57 -0300

    create Read page w/ List component

---
+++ src/pages/Read.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  q-page.row.items-center.column
+    List
+</template>
+
+<script lang="ts">
+import List from 'src/components/List.vue';
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Read',
+    components: {
+      List
+    }
+})
+</script>
