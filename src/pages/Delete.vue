543028b2abee343e78d184dfba95a6ebe99a439a
Facu bobarini <facubobarini@gmail.com>
2024-10-24 00:13:45 -0300

    create new page: Delete

---
+++ src/pages/Delete.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  q-page.column.items-center
+    SearchDelete
+</template>
+
+<script lang="ts">
+import SearchDelete from 'src/components/Search-delete.vue';
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Delete',
+  components: {
+    SearchDelete
+  }
+})
+</script>
