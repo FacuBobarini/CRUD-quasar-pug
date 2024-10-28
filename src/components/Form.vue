c7e82fddef63447e62db2be8d170af1ebbaba53c
Facu bobarini <facubobarini@gmail.com>
2024-10-28 01:49:38 -0300

    add emitForm and @submit to call it

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  q-form
+  q-form(@submit="emitForm")
     q-input(outlined v-model="formData.name" label="Nombre" hint="Coloca tu nombre" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!']").q-pt-sm
     q-input(outlined v-model="formData.lastName" label="Apellido" hint="Coloca tu apellido" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!']").q-pt-sm
     q-input(outlined v-model="formData.dni" label="DNI" hint="Coloca tu DNI" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!', val => /^([0-9])*$/.test(val) || 'Solo se permiten numeros!']").q-pt-sm 
@@ -20,7 +20,13 @@ export default defineComponent({
         dni: ''
       }
     };
-  }
+  },
+
+  methods: {
+    emitForm() {
+      this.$emit('formSent', this.formData);
+    }
+}
 });
 </script>
     
