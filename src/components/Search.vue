eab91cb4f15117efa96c7b70d5bc898b758a31c6
Facu bobarini <facubobarini@gmail.com>
2024-10-29 00:41:51 -0300

    add emitData method

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,7 +3,7 @@
     q-input(bottom-slots outlined v-model="data.id" label="Ingresar ID" hint="Buscar por id")
         template(v-slot:append)
           q-icon(v-if="data.id !== ''" name="close" @click="data.id = ''" class="cursor-pointer")
-          q-btn(round dense flat icon="send" @click="")
+          q-btn(round dense flat icon="send" @click="emitData")
 </template>
 
 <script lang="ts">
@@ -17,6 +17,12 @@ export default defineComponent({
         id: ''
       }
     };
+  },
+
+methods: {
+  emitData() {
+    this.$emit('dataSent', this.data);
   }
+}
 })
 </script>
