12f8f6b6931bd88ee48a4103f23e445225ad7259
Facu bobarini <facubobarini@gmail.com>
2024-10-31 23:53:04 -0300

    replace component searchDelete to Search- change class in q-page tag

--- src/pages/Delete.vue
+++ src/pages/Delete.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-  q-page.column.items-center
-    SearchDelete
+  q-page.column.items-center.justify-left.q-pt-md.bg-grey-4
+    Search
 </template>
 
 <script lang="ts">
-import SearchDelete from 'src/components/Search-delete.vue';
+import Search from 'src/components/Search.vue';
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'Delete',
   components: {
-    SearchDelete
+    Search
   }
 })
 </script>
