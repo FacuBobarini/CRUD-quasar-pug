2e79caaa30415ebbbefa9b9bda653fe4ad6f4f60
Facu bobarini <facubobarini@gmail.com>
2024-11-09 23:48:09 -0300

    add event to Search component to trigger deleteRegister and add fucntion deleteRegister w/ fetch and notify

--- src/pages/Delete.vue
+++ src/pages/Delete.vue
@@ -1,15 +1,35 @@
 <template lang="pug">
   q-page.column.items-center.justify-left.q-pt-md.bg-grey-4
-    Search
+    Search(@dataSent="deleteRegister")
 </template>
 
 <script lang="ts">
+import axios from 'axios';
+import { useQuasar } from 'quasar';
+import { AxiosResponse } from 'src/components/interfaces/personInterface';
 import Search from 'src/components/Search.vue';
 import { defineComponent } from 'vue'
+
+const $q = useQuasar()
+
 export default defineComponent({
   name: 'Delete',
   components: {
     Search
+  },
+
+  methods: {
+    async deleteRegister(id:string){
+      try{
+      const registerDeleted: AxiosResponse = await axios.delete(`https://crud-nestjs-icek.onrender.com/person/${id}`)
+
+      this.$q.notify({
+        message: 'Se elimino el registro'
+      })
+    }catch(error){
+      console.error("Error: ", error)
+    }
+    }
   }
 })
 </script>
