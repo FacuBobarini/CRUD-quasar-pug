674525cf3862f506727ef24b9a92ace1ca1d0d23
Facu bobarini <facubobarini@gmail.com>
2024-11-14 00:14:15 -0300

    add props: hintText to hint and rules - rulesDNI to lazy-rules :rules

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,40 +1,53 @@
 <template lang="pug">
   q-form(@submit="emitForm")
-    q-input(outlined v-model="formData.name" label="Nombre" hint="Coloca tu nombre" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!']").q-pt-sm
-    q-input(outlined v-model="formData.lastName" label="Apellido" hint="Coloca tu apellido" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!']").q-pt-sm
-    q-input(outlined v-model="formData.dni" label="DNI" hint="Coloca tu DNI" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo esta vacio!', val => /^([0-9])*$/.test(val) || 'Solo se permiten numeros!']").q-pt-sm 
+    q-input(outlined v-model="formData.name" label="Nombre" :hint="`${hintText} nombre`" lazy-rules :rules="rules").q-pt-sm
+    q-input(outlined v-model="formData.lastName" label="Apellido" :hint="`${hintText} apellido`" lazy-rules :rules="rules").q-pt-sm
+    q-input(outlined v-model="formData.dni" label="DNI" :hint="`${hintText} DNI`" lazy-rules :rules="rulesDNI").q-pt-sm 
     q-btn(unelevated  color="primary" type="submit").q-ma-sm {{label}}
 </template>
     
 <script lang="ts">
+import { ValidationRule } from 'quasar';
 import { defineComponent, PropType } from 'vue';
+import { FormInterface, UpdateFormInterface } from './interfaces/formInterface';
   
 export default defineComponent({
   name: 'Form',
 
   data() {
     return {
-      formData: {
-        name: '',
-        lastName: '',
-        dni: ''
-      }
+      formData: {} as FormInterface | UpdateFormInterface
     };
   },
 
   props: {
     label: {
-      type: String as PropType<string>,
+      type: String,
       required: true
+    },
+
+    hintText: {
+      type: String,
+      default: ''
+    },
+
+    rules: {
+      type: [] as PropType<[ValidationRule]|[ValidationRule,ValidationRule]>
+    },
+
+    rulesDNI: {
+      type: [] as PropType<[ValidationRule]|[ValidationRule,ValidationRule]>
     }
   },
 
   methods: {
+
+    hintForm(submit:string) {
+      
+    },
+
     emitForm() {
       this.$emit('formSent', this.formData);
-      this.formData = {name: '',
-        lastName: '',
-        dni: ''}
     }
 }
 });
